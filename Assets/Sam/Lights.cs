60e78f0a182959704b4a2347e18894231e818247
spbeal <62578556+spbeal@users.noreply.github.com>
2024-10-13 20:11:17 -0700

    Start LightSystem Class

--- Assets/Sam/Lights.cs
+++ Assets/Sam/Lights.cs
@@ -35,5 +35,90 @@ public class Lights : MonoBehaviour
         //myLight.color = Color.Lerp(Lerp(green, Lerp(yellow, white)), Lerp(red, Lerp(blue, black)), t);
         //myLight.color = Color.Lerp(green, white, t);
         //myLight.color = Color.Lerp(black, yellow, t);
+
+
+    }
+}
+
+// Class with all the light functionality. 
+
+public class LightSystem
+{
+    // Start is called before the first frame update
+/*    LightSystem()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            WhiteLight a = new WhiteLight();
+        }
+        for (int i = 0; i < 10; i++)
+        {
+            ColorLight a = new ColorLight();
+        }
+    }*/
+
+}
+
+public class ColorLight : LightSystem
+{
+    ColorLight()
+    {
+        // Make a game object
+        GameObject lightGameObject = new GameObject("ColorLight");
+
+        // Add the light component
+        Light lightComp = lightGameObject.AddComponent<Light>();
+
+        // Set color and position
+        lightComp.color = Color.red;
+
+        // Set the position (or any transform property)
+        lightGameObject.transform.position = new Vector3(0,0,0);
+    }
+    ColorLight(int x, int y, int z, Color color)
+    {
+        // Make a game object
+        GameObject lightGameObject = new GameObject("ColorLight");
+
+        // Add the light component
+        Light lightComp = lightGameObject.AddComponent<Light>();
+
+        // Set color and position
+        lightComp.color = color;
+
+        // Set the position (or any transform property)
+        lightGameObject.transform.position = new Vector3(x, y, z);
+    }
+}
+
+public class WhiteLight : LightSystem
+{
+    WhiteLight()
+    {
+        // Make a game object
+        GameObject lightGameObject = new GameObject("WhiteLight");
+
+        // Add the light component
+        Light lightComp = lightGameObject.AddComponent<Light>();
+
+        // Set color and position
+        lightComp.color = Color.white;
+
+        // Set the position (or any transform property)
+        lightGameObject.transform.position = new Vector3(0,0,0);
+    }
+    WhiteLight(int x, int y, int z)
+    {
+        // Make a game object
+        GameObject lightGameObject = new GameObject("WhiteLight");
+
+        // Add the light component
+        Light lightComp = lightGameObject.AddComponent<Light>();
+
+        // Set color and position
+        lightComp.color = Color.white;
+
+        // Set the position (or any transform property)
+        lightGameObject.transform.position = new Vector3(x, y, z);
     }
 }
