c97c590162542487af4a6b77c7de73270a903223
Ashton Pomerantz <ashtonpomerantz@gmail.com>
2024-10-31 00:23:00 -0700

    changed abstract to virtual. oops

--- Assets/Ashton/Scripts/Skills/skill.cs
+++ Assets/Ashton/Scripts/Skills/skill.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Skill
+public class Skill
 {
-    public abstract void Apply(PlayerStats playerStats);
+    public virtual void Apply(PlayerStats playerStats);
 }
 
 
