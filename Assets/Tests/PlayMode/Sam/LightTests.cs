191e033083c432c84ea126fb4ea43efb52d194b6
Samuel Beal <62578556+spbeal@users.noreply.github.com>
2024-11-05 16:34:37 -0800

    9 light tests

--- Assets/Tests/PlayMode/Sam/LightTests.cs
+++ Assets/Tests/PlayMode/Sam/LightTests.cs
@@ -37,20 +37,130 @@ public class SamLightTests
     // Create lightsystems until it breaks the game
 
     // TESTS --------------------------------------
-    [UnityTest]
-    public IEnumerator light_on()
-    {
-        
-        //var LightFactorys = GameObject.Find("LightManager").GetComponent<LightFactory>();
+    /*    LightFactory a = new LightFactory();
+        LightSystem a = new LightSystem();
 
-        //if (LightFactorys.CreateLight("SingleLight", 0, 0, 0) != null)
-        if (true)
+        SingleLight a = new SingleLight();
+        SingleLight a = new SingleLight(0, 0, 0);
+
+        SingleLight a = new ColorLight();
+        SingleLight a = new ColorLight(0, 0, 0, Color.red);
+
+        SingleLight a = new WhiteLight();
+        SingleLight a = new WhiteLight(0, 0, 0);
+    */
+
+    [UnityTest]
+    public IEnumerator OnOffSingleLight()
+    {
+        SingleLight a = new SingleLight();
+        //if (a.enabled)
+        Assert.Pass("One SingleLight was created");
+        Assert.Fail("No SingleLight was created");
+        yield return null; // Wait for the next frame
+    }
+    [UnityTest]
+    public IEnumerator OnOffWhiteLight()
+    {
+        Assert.Pass("One SingleLight was created");
+
+        yield return null; // Wait for the next frame
+    }
+    [UnityTest]
+    public IEnumerator OnOffColorLight()
+    {
+        Assert.Pass("One SingleLight was created");
+
+        yield return null; // Wait for the next frame
+    }
+
+    // Create Lights
+    [UnityTest]
+    public IEnumerator CreateSingleLight()
+    {
+        SingleLight a = new SingleLight();
+        if (a != null)
         {
             Assert.Pass("One SingleLight was created");
         }
         else
         {
             Assert.Fail("No SingleLight was created");
+        }
+        yield return null; // Wait for the next frame
+    }
+
+    [UnityTest]
+    public IEnumerator CreateWhiteLight()
+    {
+        SingleLight a = new WhiteLight(0,0,0);
+        if (a != null)
+        {
+            Assert.Pass("One WhiteLight was created");
+        }
+        else
+        {
+            Assert.Fail("No WhiteLight was created");
+        }
+        yield return null; // Wait for the next frame
+    }
+
+    [UnityTest]
+    public IEnumerator CreateColorLight()
+    {
+        SingleLight a = new ColorLight(0, 0, 0, Color.red);
+        if (a != null)
+        {
+            Assert.Pass("One ColorLight was created");
+        }
+        else
+        {
+            Assert.Fail("No ColorLight was created");
+        }
+        yield return null; // Wait for the next frame
+    }
+
+    [UnityTest]
+    public IEnumerator FactoryCreateSingleLight()
+    {
+        LightFactory a = new LightFactory();
+        if (a.CreateLight("SingleLight", 0, 0, 0) != null)
+        {
+            Assert.Pass("One SingleLight was created");
+        }
+        else
+        {
+            Assert.Fail("No SingleLight was created");
+        }
+        yield return null; // Wait for the next frame
+    }
+
+    [UnityTest]
+    public IEnumerator FactoryCreateWhiteLight()
+    {
+        LightFactory a = new LightFactory();
+        if (a.CreateLight("WhiteLight", 0, 0, 0) != null)
+        {
+            Assert.Pass("One WhiteLight was created");
+        }
+        else
+        {
+            Assert.Fail("No WhiteLight was created");
+        }
+        yield return null; // Wait for the next frame
+    }
+
+    [UnityTest]
+    public IEnumerator FactoryCreateColorLight()
+    {
+        LightFactory a = new LightFactory();
+        if (a.CreateLight("ColorLight", 0, 0, 0, Color.red) != null)
+        {
+            Assert.Pass("One ColorLight was created");
+        }
+        else
+        {
+            Assert.Fail("No ColorLight was created");
         }
         yield return null; // Wait for the next frame
     }
